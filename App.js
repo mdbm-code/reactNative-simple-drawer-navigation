2fec4e006b6f9e161385dbadad8b265d7a9dc15e
Azat Abzalov <mdbm@yandex.kz>
2024-12-25 14:40:48 +0300

    add some style

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Drawer = createDrawerNavigator();
 
@@ -9,10 +10,38 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator
-      //initialRouteName='User' //если необходимо явно указать начальную страницу
+        //initialRouteName='User' //если необходимо явно указать начальную страницу
+        screenOptions={{
+          //стилизация для всех страниц
+          drawerActiveBackgroundColor: '#f0e1ff',
+          drawerActiveTintColor: '#3c0a6b',
+          drawerStyle: {
+            backgroundColor: '#ccc',
+          },
+        }}
       >
-        <Drawer.Screen name='Welcome' component={WelcomeScreen} />
-        <Drawer.Screen name='User' component={UserScreen} />
+        <Drawer.Screen
+          name='Welcome'
+          component={WelcomeScreen}
+          options={{
+            //стилизация персонально для страницы Welcome
+            headerStyle: { backgroundColor: '#3c0a6b' },
+            headerTintColor: 'white',
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name='home' color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name='User'
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons size={size} name='person' />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,11 +1,20 @@
 import { View, Text, Button, StyleSheet } from 'react-native';
 
-function UserScreen() {
+//компоненты, зарегистрированные в <Drawer.Navigator> см.файл App.js
+//автоматические принимают route, navigation в качестве входящих параметров
+function UserScreen({ route, navigation }) {
+  //способ вызвать меню с помощью кнопки на странице
+  function openDrawerHandler() {
+    //если в момент нажатия он уже открыт, то закроется
+    //если в момент нажатия закрыт, то откроется
+    navigation.toggleDrawer();
+  }
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"User"</Text> screen!
       </Text>
+      <Button title='Open Drawer' onPress={openDrawerHandler} />
     </View>
   );
 }
